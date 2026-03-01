a83af210506bfc2cd6e91566cda8c076742a676b
Duevyn Cooke <duevync@gmail.com>
2026-03-01 15:28:56 -0800

    impl mbc1 controller all sizes. Largest test size was Links Awakening 512KiB and played successfully (except for banking mode and multi cartridge)

--- bus.c
+++ bus.c
@@ -5,16 +5,24 @@
 #include <stdio.h>
 #include "logger.h"
 
-//#define fprintf(stderr, ...) ((void)0)
+#define fprintf(stderr, ...) ((void)0)
 
 uint8_t bus_read(struct GameBoy *gb, uint16_t addr)
 {
+	if (addr == (P1 + IO_OFFSET))
+		fprintf(logfile,
+			"%lu (%lu):rd P1 ctrl=0x%02x, btns=%02x, dpad=%02x\n",
+			gb->cpu.cnt, gb->cpu.dcnt / 70224, joypad_ctrl,
+			joypad_dpad, joypad_buttons);
 	if (addr < 0x8000)
-		return cart_read(&gb->crt, addr);
+		return mbc_rd[gb->crt.type](&gb->crt, addr);
 
 	if (addr >= 0x8000 && addr < 0xA000) {
-		if ((io_map[LCDC] & 0x80) && (gb->ppu.mode == DRAW)) {
-			fprintf(stderr, "Cannot read VRAM. PPU mode %d\n",
+		if (!gb->dma.active && (io_map[LCDC] & 0x80) &&
+		    (gb->ppu.mode == DRAW)) {
+			fprintf(logfile,
+				"%lu (%lu): ALERT Cannot RD VRAM. PPU in mode %d\n",
+				gb->cpu.cnt, gb->cpu.dcnt / 70224,
 				gb->ppu.mode);
 			return 0xFF;
 		}
@@ -22,16 +30,19 @@ uint8_t bus_read(struct GameBoy *gb, uint16_t addr)
 	}
 
 	if (addr >= 0xA000 && addr <= 0xBFFF) //cart ram
-		return cart_read(&gb->crt, addr);
+		return mbc_rd[gb->crt.type](&gb->crt, addr);
 
 	if (addr >= 0xC000 && addr <= 0xDFFF) //wram
 		return gb->wram[addr - 0xC000];
 
 	if (addr >= 0xFE00 && addr <= 0xFE9F) {
 		//oam
-		if (gb->ppu.mode == OAM || gb->ppu.mode == DRAW) {
+		if (!gb->dma.active && (io_map[LCDC] & 0x80) &&
+		    (gb->ppu.mode == OAM || gb->ppu.mode == DRAW)) {
 			//dma access allowed
-			fprintf(stderr, "Cannot read OAM. PPU mode %d\n",
+			fprintf(logfile,
+				"%lu (%lu): ALERT Cannot RD OAM. PPU in mode %d\n",
+				gb->cpu.cnt, gb->cpu.dcnt / 70224,
 				gb->ppu.mode);
 			return 0xFF;
 		}
@@ -52,77 +63,36 @@ uint8_t bus_read(struct GameBoy *gb, uint16_t addr)
 
 void bus_write(struct GameBoy *gb, uint16_t addr, uint8_t data)
 {
-	if (addr == 0xFF80) {
-		if (logfile) {
-			fprintf(logfile,
-				"%lu: [MEM] WRITE FF80 <= %02X (AT PC:%04X)\n",
-				gb->cpu.cnt, data, gb->cpu.pc);
-		}
-	}
 	if (addr < 0x8000) { //rom
-		cart_write(&gb->crt, addr, data);
-		fprintf(stderr, "  ALERT wr @rom 0x%04x 0x%02x ", addr, data);
-		return;
-	}
-
-	if (addr >= 0x8000 && addr < 0xA000) { //vram
+		mbc_wr[gb->crt.type](&gb->crt, addr, data);
+	} else if (addr >= 0x8000 && addr < 0xA000) { //vram
 		if ((io_map[LCDC] & 0x80) && (gb->ppu.mode == DRAW)) {
-			fprintf(stderr,
-				"ALERT: Cannot write to VRAM. PPU in mode %d ",
+			fprintf(logfile,
+				"%lu (%lu): ALERT Cannot write to VRAM. PPU in mode %d\n",
+				gb->cpu.cnt, gb->cpu.dcnt / 70224,
 				gb->ppu.mode);
 			return;
 		}
-
-		fprintf(stderr, "  wr @vram 0x%04x 0x%02x ", addr, data);
 		gb->ppu.vram[addr - 0x8000] = data;
-		return;
-	}
-
-	if (addr >= 0xA000 && addr <= 0xBFFF) { // cart ram
-		//cart ram
-		fprintf(stderr, "  wr @crt ram 0x%04x 0x%02x ", addr, data);
-		cart_write(&gb->crt, addr, data);
-		return;
-	}
-
-	if (addr >= 0xC000 && addr <= 0xDFFF) { //wram
-		//wram
-		//fprintf(stderr, "  wr @wram 0x%04x 0x%02x ", addr, data);
-		fprintf(stderr, "  wr @wram 0x%04x 0x%02x ", addr, data);
+	} else if (addr >= 0xA000 && addr <= 0xBFFF) { // cart ram
+		mbc_wr[gb->crt.type](&gb->crt, addr, data);
+	} else if (addr >= 0xC000 && addr <= 0xDFFF) { //wram
 		gb->wram[addr - 0xC000] = data;
-		return;
-	}
-
-	if (addr >= 0xFE00 && addr <= 0xFE9F) { //oam
-		//oam
+	} else if (addr >= 0xFE00 && addr <= 0xFE9F) { //oam
 		if ((io_map[LCDC] & 0x80) &&
 		    ((gb->ppu.mode == OAM) || (gb->ppu.mode == DRAW))) {
-			//dma access allowed
-			fprintf(stderr, "Cannot write to OAM. PPU in mode %d\n",
+			fprintf(logfile,
+				"%lu (%lu): ALERT Cannot write to OAM. PPU in mode %d\n",
+				gb->cpu.cnt, gb->cpu.dcnt / 70224,
 				gb->ppu.mode);
 			return;
 		}
-		fprintf(stderr, "  wr @oam 0x%04x 0x%02x ", addr, data);
 		gb->ppu.oam[addr - 0xFE00] = data;
-		return;
-	}
-
-	if (addr >= 0xFEA0 && addr <= 0xFEFF) //PROHIBITED
-		return;
-
-	if ((addr >= 0xFF00 && addr <= 0xFF7F) || (addr == 0xFFFF)) { //io
+	} else if ((addr >= 0xFF00 && addr <= 0xFF7F) || (addr == 0xFFFF)) {
 		io_wr(gb, addr, data);
-		return;
-	}
-
-	if (addr >= 0xFF80 && addr <= 0xFFFE) { //hram
-		//hram
-		fprintf(stderr, "  wr @hram 0x%04x 0x%02x ", addr, data);
-
+	} else if (addr >= 0xFF80 && addr <= 0xFFFE) { //hram
 		gb->hram[addr - 0xFF80] = data;
-		return;
 	}
 
 	fprintf(stderr, "-- Error writing to 0x%04x (bus.c)\n", addr);
-	exit(EXIT_FAILURE);
 }
--- cartridge.h
+++ cartridge.h
@@ -5,24 +5,62 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+enum i_cart_head { CART_TYPE = 0x147, ROM_SZ = 0x148, RAM_SZ = 0x149 };
+
+enum cart_type {
+	ROM_ONLY = 0x00,
+	MBC1 = 0x01,
+	MBC1_RAM = 0x02,
+	MBC1_RAM_BATTERY = 0x03,
+	MBC2 = 0x05,
+	MBC2_BATTERY = 0x06,
+	ROM_RAM_11 = 0x08,
+	ROM_RAM_BATTERY_11 = 0x09,
+	MMM01 = 0x0B,
+	MMM01_RAM = 0x0C,
+	MMM01_RAM_BATTERY = 0x0D,
+	MBC3_TIMER_BATTERY = 0x0F,
+	MBC3_TIMER_RAM_BATTERY_12 = 0x10,
+	MBC3 = 0x11,
+	MBC3_RAM_12 = 0x12,
+	MBC3_RAM_BATTERY_12 = 0x13,
+	MBC5 = 0x19,
+	MBC5_RAM = 0x1A,
+	MBC5_RAM_BATTERY = 0x1B,
+	MBC5_RUMBLE = 0x1C,
+	MBC5_RUMBLE_RAM = 0x1D,
+	MBC5_RUMBLE_RAM_BATTERY = 0x1E,
+	MBC6 = 0x20,
+	MBC7_SENSOR_RUMBLE_RAM_BATTERY = 0x22,
+	POCKET_CAMERA = 0xFC,
+	BANDAI_TAMA5 = 0xFD,
+	HuC3 = 0xFE,
+	HuC1_RAM_BATTERY = 0xFF
+};
+
 typedef struct Cartridge {
-	uint8_t *rom_data;
-	size_t rom_size;
+	uint8_t *rom;
+	size_t rom_sz;
 
-	uint8_t *cart_ram;
-	size_t ram_size;
+	uint8_t *ram;
+	size_t ram_sz;
 
-	struct {
-		uint8_t rom_bank;
-		uint8_t ram_bank;
-		bool ram_enabled;
-		uint8_t mode;
-	} mbc;
+	uint8_t rom_bk;
+	uint8_t ram_bk;
+	bool ram_prms;
+	uint8_t bk_md;
+	uint8_t n_rom_bk;
+	uint8_t n_rg_bits;
+
+	enum cart_type type;
 
 } Cartridge;
 
-uint8_t cart_read(struct Cartridge *crt, uint16_t addr);
-void cart_write(struct Cartridge *crt, uint16_t addr, uint8_t data);
+typedef uint8_t (*cart_rd)(struct Cartridge *c, uint16_t addr);
+typedef void (*cart_wr)(struct Cartridge *c, uint16_t addr, uint8_t data);
+extern cart_rd mbc_rd[];
+extern cart_wr mbc_wr[];
+
 void cart_load(struct Cartridge *crt, const char *path);
 
 #endif
